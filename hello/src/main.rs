855b7ab437ac7f4cc1b095efddfba338b20fc51a
yusong-offx <hssong1024@gmail.com>
2023-01-20 13:13:11 +0900

    5430

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,38 +1,44 @@
-use std::{
+use std:: {
     io::{self, Read},
     collections::{VecDeque}
 };
+
 fn main() {
     let mut buffer: String = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
-    let mut info_iter = buffer
-        .split_ascii_whitespace()
-        .map(|x| x.parse::<usize>().unwrap());
-    let origin_count = info_iter.next().unwrap();
-    let mut origin_list: VecDeque<usize> = (1..=origin_count).collect();
-    info_iter.next().unwrap();
-    let extract_list: Vec<usize> = info_iter.collect();
-    let mut work_count: usize = 0;
-    for now in extract_list {
-        if *origin_list.front().unwrap() != now {
-            let mid: usize = (origin_list.len() / 2) + 1;
-            let mut index_of: usize = 0;
-            for i in 0..origin_list.len() {
-                if now == origin_list[i] {
-                    index_of = i;
-                    break;
-                }
-            }
-            if index_of < mid {
-                work_count += index_of;
-                origin_list.rotate_left(index_of);
+    let trim_str:&[_] = &['[', ']'];
+    let mut info_iter = buffer.split_ascii_whitespace();
+    let x: usize = info_iter.next().unwrap().parse().unwrap();
+    'outer:for _ in 0..x {
+        let cmd = info_iter.next().unwrap().chars();
+        info_iter.next();
+        let mut head: bool = true;
+        let mut container: VecDeque<_> = info_iter.next().unwrap()
+            .trim_matches(trim_str)
+            .split(',')
+            .collect();
+        if container[0] == "" {
+            container.pop_front();
+        }
+        for c in cmd {
+            if c == 'R' {
+                head = !head;
             } else {
-                let tmp = origin_list.len() - index_of;
-                work_count += tmp;
-                origin_list.rotate_right(tmp);
+                if container.is_empty() {
+                    println!("error");
+                    continue 'outer; 
+                }
+                match head {
+                    true => container.pop_front().unwrap(),
+                    false => container.pop_back().unwrap(),
+                };
             }
         }
-        origin_list.pop_front();
+        if !head {
+            container.make_contiguous().reverse();
+        }
+        let answer: Vec<_> = container.into();
+        println!("[{}]", answer.join(","));
     }
-    print!("{work_count}");
+
 }
