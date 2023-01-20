c506bc799044c387b8320dae6c90aadf89045e88
yusong-offx <hssong1024@gmail.com>
2023-01-21 08:20:23 +0900

    3986

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,44 +1,25 @@
 use std:: {
     io::{self, Read},
-    collections::{VecDeque}
 };
 
 fn main() {
     let mut buffer: String = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
-    let trim_str:&[_] = &['[', ']'];
-    let mut info_iter = buffer.split_ascii_whitespace();
-    let x: usize = info_iter.next().unwrap().parse().unwrap();
-    'outer:for _ in 0..x {
-        let cmd = info_iter.next().unwrap().chars();
-        info_iter.next();
-        let mut head: bool = true;
-        let mut container: VecDeque<_> = info_iter.next().unwrap()
-            .trim_matches(trim_str)
-            .split(',')
-            .collect();
-        if container[0] == "" {
-            container.pop_front();
-        }
-        for c in cmd {
-            if c == 'R' {
-                head = !head;
+    let mut info = buffer.split_ascii_whitespace();
+    info.next();
+    let mut answer = 0;
+    for s in info {
+        let mut stack: Vec<char> = Vec::with_capacity(s.len());
+        for c in s.chars() {
+            if !stack.is_empty() && *stack.last().unwrap() == c {
+                stack.pop();
             } else {
-                if container.is_empty() {
-                    println!("error");
-                    continue 'outer; 
-                }
-                match head {
-                    true => container.pop_front().unwrap(),
-                    false => container.pop_back().unwrap(),
-                };
+                stack.push(c);
             }
         }
-        if !head {
-            container.make_contiguous().reverse();
+        if stack.is_empty() {
+            answer += 1;
         }
-        let answer: Vec<_> = container.into();
-        println!("[{}]", answer.join(","));
     }
-
+    print!("{answer}");
 }
