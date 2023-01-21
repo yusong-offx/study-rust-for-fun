ee6f325ce0dcff46769739ff1930e9a97c47252a
yusong-offx <hssong1024@gmail.com>
2023-01-21 10:25:48 +0900

    2540

---
+++ hello/src/solve/3986.rs
@@ -0,0 +1,25 @@
+use std:: {
+    io::{self, Read},
+};
+
+fn main() {
+    let mut buffer: String = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let info = buffer.split_ascii_whitespace();
+    info.next();
+    let mut answer = 0;
+    for s in info {
+        let mut stack: Vec<char> = Vec::with_capacity(s.len());
+        for c in s.chars() {
+            if !stack.is_empty() && *stack.last().unwrap() == c {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        if stack.is_empty() {
+            answer += 1;
+        }
+    }
+    print!("{answer}");
+}
