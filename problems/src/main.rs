72f90104c45a18fef2e676c1c92f4c1e8ff48cb6
yusong-offx <hssong1024@gmail.com>
2023-05-09 20:37:56 +0900

    leetcode

--- problems/src/main.rs
+++ problems/src/main.rs
@@ -1,25 +1,3 @@
-struct Solution;
-
-use std::collections::HashMap;
-
-impl Solution {
-    pub fn first_uniq_char(s: String) -> i32 {
-        let mut map = HashMap::new();
-        for c in s.chars() {
-            map.entry(c)
-                .and_modify(|v| *v = true)
-                .or_default();
-        }
-        for (i, c) in s.chars().enumerate() {
-            if !*map.get(&c).unwrap() {
-                return i as i32
-            }
-        }
-        -1
-    }
-}
-
 fn main() {
-    assert_eq!(Solution::first_uniq_char(String::from("leetcode")), 0);
-    assert_eq!(Solution::first_uniq_char(String::from("loveleetcode")), 2);
+    println!("hello world!");
 }
